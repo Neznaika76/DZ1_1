8fb3027a3269bd8df6886753c507a8c7af6167ef
PavelMikhailov <n9032692530@gmail.com>
2022-10-08 13:11:40 +0300

    Домашнее задание 1

--- main.py
+++ main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+# Задача: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+# 6 -> да
+# 7 -> да
+# 1 -> нет
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# x=int(input("Введите цифру: "))
+# if x>0 and x<8:
+#     if x==6 or x==7:
+#         print("Этот день выходной")
+#     else:
+#         print("Этот день рабочий")
+# else:
+#     print("В следующий раз введите число от 1 до 7")
+
+# Напишите программу для. проверки истинности
+# утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             if (not(x and y and z)) == (not(x) or not(y) or not(z)):
+#                 print(x,y,z)
+#                 print(not(x and y and z),  "=", not(x) or not(y) or not(z))
+
+# Напишите программу, которая принимает на вход координаты точки (X и Y),
+# причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+# Пример:
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
+
+# x=int(input("Введите координату Х: "))
+# y=int(input("Введите координату Y: "))
+# if x!=0 and y!=0:
+#     if x>0:
+#         if y>0:
+#             print("1 четверть")
+#         else:
+#             print("4 четверть")
+#     else:
+#         if y>0:
+#             print("2 четверть")
+#         else:
+#             print("3 четверть")
+# else:
+#     print("Не вводите 0")
+
+# Напишите программу, которая по заданному номеру
+# четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+# x=int(input("Введите цифру: "))
+# if x==1:
+#     print("x>0 & y>0")
+# else:
+#     if x == 2:
+#         print("x<0 & y>0")
+#     else:
+#         if x == 3:
+#             print("x<0 & y<0")
+#         else:
+#             if x == 4:
+#                 print("x>0 & y<0")
+#             else:
+#                 print("Введите чило от 1 до 4")
+
+# Напишите программу, которая принимает на вход координаты двух точек и находит
+# расстояние между ними в 2D пространстве.
+# Пример:
+# - A (3,6); B (2,1) -> 5,09
+# - A (7,-5); B (1,-1) -> 7,21
+
+x1, y1 = map(int, input("Введите координаты 1 точки: ").split())
+x2, y2 = map(int, input("Введите координаты 2 точки: ").split())
+print(((x1-x2)**2+(y1-y2)**2)**(0.5))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
