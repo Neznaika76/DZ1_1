b06d5dd05eee39585759f4c371df5bd7bfce4381
PavelMikhailov <n9032692530@gmail.com>
2022-10-21 07:48:11 +0300

    Домашнее задание 4 Задача 5

--- Tasks 4.py
+++ Tasks 4.py
@@ -68,28 +68,94 @@
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
+# import random
+#
+#
+# def add_elements(x):
+#     for _ in range(x):
+#         elements.append(random.randint(0, 20))
+#     print(elements)
+#
+#
+# n = int(input('Введите число: '))
+# elements = []
+# add_elements(n)
+#
+# path = 'file.txt'
+# data = open(path, 'w')
+# for i in range(n):
+#     if i == n-1 and elements[i] != 0:
+#         # print(f'{elements[i]}')
+#         data.writelines(f'{elements[i]}')
+#     elif elements[i] != 0:
+#         data.writelines(f'{elements[i]}x{n-1-i} + ')
+#         # print(f'{elements[i]}x{n-1-i} +')
+#     i+= 1
+#
+# data.close()
+#
+# Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
 import random
+import re
 
 
-def add_elements(x):
+def add_elements(elements, x):
     for _ in range(x):
         elements.append(random.randint(0, 20))
-    print(elements)
+    # print(elements)
 
 
 n = int(input('Введите число: '))
-elements = []
-add_elements(n)
+elements1 = []
+elements2 = []
+add_elements(elements1, n)
+add_elements(elements2, n)
 
-path = 'file.txt'
+path = 'file1.txt'
 data = open(path, 'w')
 for i in range(n):
-    if i == n-1 and elements[i] != 0:
-        # print(f'{elements[i]}')
-        data.writelines(f'{elements[i]}')
-    elif elements[i] != 0:
-        data.writelines(f'{elements[i]}x{n-1-i} + ')
-        # print(f'{elements[i]}x{n-1-i} +')
+    if i == n-1 and elements1[i] != 0:
+        data.writelines(f'{elements1[i]}')
+    elif elements1[i] != 0:
+        data.writelines(f'{elements1[i]}x{n-1-i}+')
     i+= 1
+data.close()
 
+path = 'file2.txt'
+data = open(path, 'w')
+for i in range(n):
+    if i == n-1 and elements2[i] != 0:
+        data.writelines(f'{elements2[i]}')
+    elif elements2[i] != 0:
+        data.writelines(f'{elements2[i]}x{n-1-i}+')
+    i+= 1
+data.close()
+
+elements_new1 = []
+path = 'file1.txt'
+data = open(path, 'r')
+elements_new1 = data.read()
+# print(elements_new1)
+data.close()
+elements_new1 = re.findall('\d+', elements_new1)
+
+elements_new2 = []
+path = 'file2.txt'
+data = open(path, 'r')
+elements_new2 = data.read()
+# print(elements_new2)
+data.close()
+elements_new2 = re.findall('\d+', elements_new2)
+
+print("Полученный многочлен записан в файл 'file3.txt'")
+
+path = 'file3.txt'
+data = open(path, 'w')
+j = 0
+for i in range(0, len(elements_new1), 2):
+    if i == len(elements_new1)-1 and elements_new1[i] != 0:
+        data.writelines(f'{elements_new1[i]}')
+    elif elements_new1[i] != 0:
+        data.writelines(f'{int(elements_new1[i])+int(elements_new2[i])}x{len(elements_new1)//2-j} + ')
+        j += 1
 data.close()
