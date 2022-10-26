78b9033c1598d7b441494be715a570089a0a8a9d
PavelMikhailov <n9032692530@gmail.com>
2022-10-26 18:03:29 +0300

    Домашнее задание 5. Задача 2

--- Tasks 5.py
+++ Tasks 5.py
@@ -4,8 +4,90 @@
 # lines = lines.replace('абв', '')
 # print(lines)
 
+# Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
+# Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
+# Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому
+# игроку, чтобы забрать все конфеты у своего конкурента?
+import random
 
 
+def rnd_turn():  # Функция определения первого хода
+    print(f'Выбор очередности ходов: ')
+    print(f'Введите 0, если ходить первым будет Вы')
+    print(f'Введите 1, если ходить первым будет компьютер')
+    print(f'Введите 2, чтобы выбрать очередность ходов случайным образом')
+    while True:
+        n = input('> ')
+        if n.isdigit():
+            n = int(n)
+            if n > 2 or n < 0:
+                print(f'Введите число 0, 1 или 2')
+            else:
+                if n == 2:
+                    n = random.randint(0, 1)
+                if n == 0:
+                    print(f'Первым будет ходить игрок!')
+                else:
+                    print(f'Первым будет ходить компьютер!')
+                break
+        else:
+            print(f'Вы ввели недопустимый символ. Введите число 0, 1 или 2')
+            continue
+    return n
+
+
+def turn_player(matches):
+    n = 29
+    while n > 28 or n < 1:
+        n = input('Сколько вы возьмете конфет? ')
+        if n.isdigit():
+            # n = int(n)
+            if int(n) > 28 or int(n) < 1:
+                print(f'Вы вязли недопустимое количество конфет! Возьмите от 1 до 28-и')
+                continue
+            # проверка на остаток
+            if int(n) > matches:
+                print(f'Вы взяли больше чем осталось конфет!')
+                # n = 28
+                continue
+        else:
+            print(f'Вы ввели недопустимые символы. Введите число от 1 до 28-и')
+            # n = 28
+            continue
+        n = int(n)
+    return n
+
+
+def turn_ai(matches):  # Ход компьютера
+    n = matches % 28
+if n == 0:
+        n = random.randint(1, 28)
+    return n
+
+
+matches = 2021
+count = 1
+turn = rnd_turn()
+while True:
+    print(f'\n******** Ход номер: {count} ********')
+    if turn % 2 == 0:
+        print(f'Ход игрока! Всего конфет: {matches}')
+        n = turn_player(matches)
+        print(f'Игрок взял {n} конфет.')
+    elif turn % 2 == 1:
+        print(f'Ход компьютера! Всего конфет: {matches}')
+        n = turn_ai(matches)
+        print(f'Компьютер взял {n} конфет')
+    matches -= n
+    if matches == 0:
+        if turn % 2 == 0:
+            print(f'Победил игрок!')
+        else:
+            print(f'Победил компьютер!')
+        break
+    turn += 1
+    count += 1
 # Создайте программу для игры в ""Крестики-нолики"".
 #
 # board = list(range(1, 10))
@@ -65,42 +147,42 @@
 # main()
 
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
-src = 'пример следующий пример абв слова абв которое абв повторяются сжимаем пример'
-print('Строка :' + src)
-
-pack = ''
-i = 0
-while i < len(src):
-    ln = 9
-    found = False
-    while not found and ln > 3:
-        j = i - ln
-        while j >= 0 and (i-j) < 100:
-            if src[i:i+ln] == src[j:j+ln]:
-                pack += '#%1d%2d' % (ln, (i - j))
-                i += ln
-                found = True
-                break
-            j -= 1
-        ln -= 1
-    if not found:
-        pack += src[i]
-        i += 1
-
-print('Сжатая строка: ' + pack)
-
-unpack = ''
-i = 0
-while i < len(pack):
-    if pack[i] != '#':
-        unpack += pack[i]
-        i += 1
-        continue
-    ln = int(pack[i + 1: i + 2])
-    dist = int(pack[i + 2: i + 4])
-    unpack += unpack[-dist: -dist+ln]
-    i += 4
-
-print('Распакованная сторока: ' + unpack)
-if src != unpack:
-    print('Ошибка!')
+# src = 'пример следующий пример абв слова абв которое абв повторяются сжимаем пример'
+# print('Строка :' + src)
+#
+# pack = ''
+# i = 0
+# while i < len(src):
+#     ln = 9
+#     found = False
+#     while not found and ln > 3:
+#         j = i - ln
+#         while j >= 0 and (i-j) < 100:
+#             if src[i:i+ln] == src[j:j+ln]:
+#                 pack += '#%1d%2d' % (ln, (i - j))
+#                 i += ln
+#                 found = True
+#                 break
+#             j -= 1
+#         ln -= 1
+#     if not found:
+#         pack += src[i]
+#         i += 1
+#
+# print('Сжатая строка: ' + pack)
+#
+# unpack = ''
+# i = 0
+# while i < len(pack):
+#     if pack[i] != '#':
+#         unpack += pack[i]
+#         i += 1
+#         continue
+#     ln = int(pack[i + 1: i + 2])
+#     dist = int(pack[i + 2: i + 4])
+#     unpack += unpack[-dist: -dist+ln]
+#     i += 4
+#
+# print('Распакованная сторока: ' + unpack)
+# if src != unpack:
+#     print('Ошибка!')
